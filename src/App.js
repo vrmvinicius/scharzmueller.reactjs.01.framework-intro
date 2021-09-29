f2b47c95bee076fc1d0fddfba56d708494730f23
vinicius.miglioranca <vinicius.miglioranca@cwi.com.br>
2021-09-29 00:22:31 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+   render() {
+      return (
+         <div className="App" >
+            <div className="App-header">
+               <h2>Welcome to React</h2>               
+               <p className="App-intro">
+                  To get started, edit <code>src/App.js</code> and save to reload.
+               </p>
+            </div>
+         </div>
+      );
+   }
 }
 
 export default App;
