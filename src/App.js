f89edb08a1283b3f1bdfc717bac767b7cc2a374f
vinicius.miglioranca <vinicius.miglioranca@cwi.com.br>
2021-09-29 00:28:57 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Info from './components/Info';
 
 class App extends Component {
    render() {
       return (
          <div className="App" >
-            <div className="App-header">
-               <h2>Welcome to React</h2>               
-               <p className="App-intro">
-                  To get started, edit <code>src/App.js</code> and save to reload.
-               </p>
-            </div>
+            <Info />
          </div>
       );
    }
---
+++ src/components/Info.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Info extends Component {
+   render() {
+      return (
+         <div>
+            <h1>The Info</h1>
+            <p>React is awesome!</p>
+         </div>
+      );
+   }
+}
+
+export default Info;
