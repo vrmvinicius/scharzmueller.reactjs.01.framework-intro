7b22f871f45fc0164f230b24779c405e0018c18f
vinicius.miglioranca <vinicius.miglioranca@cwi.com.br>
2021-09-29 00:56:44 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import Account from './components/Account';
+import Blog from './components/Blog';
 import Info from './components/Info';
+import User from './components/User';
 
 class App extends Component {
    render() {
       return (
-         <div className="App" >
-            <Info />
-         </div>
+         <Router>
+            <div>
+               <Route exact path="/" component={User} />
+               <Route path="/blog" component={Blog} />
+               <Route path="/account" component={Account} />               
+            </div>
+         </Router>
       );
    }
 }
---
+++ src/components/User.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Info from './Info';
+
+export default function(props){
+   return (
+      <div>
+         <h1>The User Page</h1>
+         <Info />
+      </div>
+   );
+}
